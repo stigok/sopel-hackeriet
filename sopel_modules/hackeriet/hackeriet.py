14df35c2316306673651474ddefd3413e38e6371
bezaban <2104926+bezaban@users.noreply.github.com>
2019-04-08 21:12:37 +0200

    adding hackeriet info cmd

--- sopel_modules/hackeriet/hackeriet.py
+++ sopel_modules/hackeriet/hackeriet.py
@@ -4,15 +4,15 @@ from __future__ import unicode_literals, absolute_import, division, print_functi
 
 from sopel import module
 
-
 def configure(config):
     pass
-
 
 def setup(bot):
     pass
 
+def shutdown(bot).
+    pass
 
-@module.commands('helloworld')
+@module.commands('hackeriet')
 def hello_world(bot, trigger):
-    bot.say('Hello, world!')
+    bot.say('Hackeriet is a community operated hackerspace in Oslo where people tinker with software, networks, art and hardware, learn from each other. https://hackeriet.no')
